a1ef841795a95284ddb7c6cd8431cf6ec63e96a1
Dina250206 <kozhaevadina@gmail.com>
2024-11-17 17:56:26 +0500

    app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const authRoutes = require('./routes/auth');
 const portfolioRoutes = require('./routes/portfolio');
- 
+const path = require('path');
 
 const app = express();
 
@@ -18,16 +18,27 @@ app.use(
         saveUninitialized: true,
     })
 );
+
+// Корневой маршрут
+app.get('/', (req, res) => {
+    res.redirect('/portfolio/view'); // Перенаправляем на маршрут просмотра портфолио
+});
+
+// Маршруты
 app.use('/auth', authRoutes);
 app.use('/portfolio', portfolioRoutes);
 
-
+// Настройка Express
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // Для статических файлов в папке uploads
 
 // Подключение MongoDB
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error(err));
+    .catch(err => console.error('MongoDB connection error:', err));
 
 // Запуск сервера
 const PORT = process.env.PORT || 3000;
