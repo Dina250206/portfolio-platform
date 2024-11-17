1dd18116f76ae44095d590cce486b16bf1a9bfc0
Dina250206 <kozhaevadina@gmail.com>
2024-11-17 17:53:42 +0500

    authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,18 +1,6 @@
-// middleware/authMiddleware.js
-
-// Проверка аутентификации
-const authenticate = (req, res, next) => {
-    if (req.session && req.session.user) {
-        next();
-    } else {
-        res.status(401).json({ message: 'Authentication required.' });
-    }
-};
-
-// Проверка авторизации по ролям
 const authorize = (role) => {
     return (req, res, next) => {
-        if (req.session && req.session.user && req.session.user.role === role) {
+        if (req.user && req.user.role === role) {
             next();
         } else {
             res.status(403).json({ message: 'Access denied.' });
@@ -20,4 +8,4 @@ const authorize = (role) => {
     };
 };
 
-module.exports = { authenticate, authorize };
+module.exports = { authorize };
