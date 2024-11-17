68419a0e3cf3905de87e28002385fe1e75b8e3ed
Dina250206 <kozhaevadina@gmail.com>
2024-11-17 17:55:06 +0500

    PortfolioItem.js

--- models/PortfolioItem.js
+++ models/PortfolioItem.js
@@ -1,16 +1,34 @@
 const mongoose = require('mongoose');
 
+// Схема для элемента портфолио
 const PortfolioItemSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    images: [{ type: String, required: true }],
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
+    title: {
+        type: String,
+        required: true, // Заголовок обязательный
+    },
+    description: {
+        type: String,
+        required: true, // Описание обязательное
+    },
+    images: {
+        type: [String], // Массив строк для хранения путей к изображениям
+        required: true, // Поле обязательно
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now, // Дата создания по умолчанию
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now, // Дата обновления по умолчанию
+    },
 });
 
+// Middleware для обновления поля updatedAt перед сохранением
 PortfolioItemSchema.pre('save', function (next) {
-    this.updatedAt = Date.now();
-    next();
+    this.updatedAt = Date.now(); // Обновляем updatedAt
+    next(); // Переходим к следующему шагу
 });
 
+// Экспорт модели PortfolioItem
 module.exports = mongoose.model('PortfolioItem', PortfolioItemSchema);
